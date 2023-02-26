cf0ece7369a4acff068264c4a637925caa73362d
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 15:32:46 +0530

    installed boostrap and installed react bootstrap

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-// import './App.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     HELLO WORLD
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   )
 }
