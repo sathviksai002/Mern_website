0339f3630c6aa7898ce79e4c98bdbb8e06733b44
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 16:53:43 +0530

    Table commit

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,35 +1,100 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import OurNavbar from './Components/Navbar'
 import Button from "react-bootstrap/Button"
-import Container from 'react-bootstrap/esm/Container'
+import Container from "react-bootstrap/Container"
+import Table from "react-bootstrap/Table"
+import Form from "react-bootstrap/Form"
+import Badge from "react-bootstrap/Badge"
+
 
 function App() {
 
-  // let number = 0;
-  
-  const[number, setNumber] = useState(0);
+  const [number, setNumber] = useState(0);
   const [problemList, setProblemList] = useState([]);
 
-  useEffect (function(){
+
+  useEffect(function() {
+
     fetch("https://bz-neetcode-clone.cyclic.app/api/problems?sheet=NeetCode%20All")
     .then(res => res.json())
-    .then(res => setProblemList(res));  
-  },[])
+    .then(res => setProblemList(res));
 
-  //dependency array
 
-  const increasenumber = () =>{
-    // number += 1;
-    setNumber(number+1);
+  }, [])
+
+
+  // var number = 0;
+
+  const increaseNumber = () => {
+      // number+=1;
+      // console.log(number);
+      setNumber(number+1);
   }
 
   return (
     <div className="App">
-     <OurNavbar></OurNavbar>
-     <Container>
-      {JSON.stringify(problemList)}
-     </Container>
+      <OurNavbar></OurNavbar>
+      <Container>
+      <div style={{ minWidth: "300px", overflowY: "scroll" }}>
+                    <Table
+                        striped
+                        bordered
+                        hover
+                        style={{ textAlign: "center" }}
+                    >
+                        <thead>
+                            <tr>
+                                <th>Status</th>
+                                <th>Star</th>
+                                <th>Problem</th>
+                                <th>Difficulty</th>
+                                <th>Video Solution</th>
+                                <th>Code</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {problemList.map((el) => {
+                                return  <tr key={el._id}>
+                                <td>
+                                    <Form.Check type={"checkbox"} id={`default-checkbox`} />
+                                </td>
+                                <td>
+                                    <i className="fa-regular fa-star"></i>
+                                </td>
+                                <td style={{ textAlign: "left" }}>
+                                    <a href={el.url} target={"_blank"}>
+                                        {el.title}
+                                    </a>
+                                </td>
+                                <td>
+                                    <Badge
+                                        bg={
+                                            el.difficulty === "Easy"
+                                                ? "success"
+                                                : el.difficulty === "Medium"
+                                                ? "warning"
+                                                : "danger"
+                                        }
+                                    >
+                                        {el.difficulty}
+                                    </Badge>
+                                </td>
+                                <td>
+                                    <Button variant="outline-primary">
+                                        {" "}
+                                        <i className="fa-solid fa-video"></i>
+                                    </Button>
+                                </td>
+                                <td>
+                                    <Button variant="primary"> Python </Button>
+                                </td>
+                            </tr>
+                            })}
+                        </tbody>
+                    </Table>
+                </div>
+      </Container>
     </div>
   )
 }
