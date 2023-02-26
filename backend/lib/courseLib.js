8d90e6994a23b9406e4b7a65e2d91717cf63e93f
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 15:12:12 +0530

    Read update The Courses

--- backend/lib/courseLib.js
+++ backend/lib/courseLib.js
@@ -1,10 +1,24 @@
 const courseModel = require("../models/courseModel")
 
 //read
+
 module.exports.getAllCourses = async function(){
     return await courseModel.find({});
 }
 
+
+//create the course
+
+module.exports.createCourse = async function(courseInput){
+    const course = new courseModel(courseInput);
+    await course.save();
+}
+
+//update the course
+
+module.exports.updateCourse = async function(courseId, updatedInput){
+    await courseModel.findOneAndUpdate({_id: courseId},updatedInput);
+}
 
 
 module.exports.createFirstCourse = async function(){
