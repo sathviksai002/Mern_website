9cdfcaa076fd0258d8374f42746bfe07014d9c78
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 10:50:55 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require("express");
+const express = require("express"); // creating application
 
-const app = express();
+const app = express(); // calling
+var name = "Sathvik";
 
-app.get("/",function(req,res){
-    res.send("Hello World");
+app.use(express.static("Frontend"));  //telling our app to use static files 
+
+
+app.get("/html",function(req,res){
+    res.sendFile(__dirname + "/Frontend/index.html")
 })
 
+app.get("/",function(req,res){
+    res.send("Hello World!");
+})  
+
+
 app.get("/myName",function(req,res){
-    res.send("My name is $(name)");
+    res.send(`My name is ${name}`);
 })
 
 app.listen(3000,function(){
