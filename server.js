9a0f6da58bae4bf09b2c670b21793d999ca1ee7c
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 11:00:19 +0530

    serving html file

--- server.js
+++ server.js
@@ -6,18 +6,11 @@ var name = "Sathvik";
 app.use(express.static("Frontend"));  //telling our app to use static files 
 
 
-app.get("/html",function(req,res){
-    res.sendFile(__dirname + "/Frontend/index.html")
-})
-
 app.get("/",function(req,res){
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/Frontend/index.html")
+
 })  
 
-
-app.get("/myName",function(req,res){
-    res.send(`My name is ${name}`);
-})
 
 app.listen(3000,function(){
     console.log("Server running on http://localhost:3000");
