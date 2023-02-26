6484f725ee0cd1b03130b2a76aa2b7ffac3d71c9
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 15:52:35 +0530

    Moved Navbar into new components folder.

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,34 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from "react-bootstrap/Button";
+import OurNavbar from './Components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Neetode Clone</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/courses">Courses</Nav.Link>
-            <Nav.Link href="/practice">Practice</Nav.Link>
-            <Nav.Link href="/roadmap">Roadmap</Nav.Link>
-            <Nav.Link href="/pro">Pro</Nav.Link>
-
-          </Nav>
-          <div>
-              <Button variant="outline-primary">
-                 dark
-              </Button>
-              <Button variant="primary">
-                 Sign In
-              </Button>
-          </div>
-        </Container>
-      </Navbar>
+     <OurNavbar></OurNavbar>
     </div>
   )
 }
