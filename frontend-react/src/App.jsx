c10b4411afd0a0354e8ff6abb7617878cf1312e2
T Venkata sai sathvik <79955197+sathviksai002@users.noreply.github.com>
2023-02-26 16:35:15 +0530

    fetched the 312 data from the API and used state

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import OurNavbar from './Components/Navbar'
+import Button from "react-bootstrap/Button"
+import Container from 'react-bootstrap/esm/Container'
 
 function App() {
+
+  // let number = 0;
+  
+  const[number, setNumber] = useState(0);
+  const [problemList, setProblemList] = useState([]);
+
+  useEffect (function(){
+    fetch("https://bz-neetcode-clone.cyclic.app/api/problems?sheet=NeetCode%20All")
+    .then(res => res.json())
+    .then(res => setProblemList(res));  
+  },[])
+
+  //dependency array
+
+  const increasenumber = () =>{
+    // number += 1;
+    setNumber(number+1);
+  }
 
   return (
     <div className="App">
      <OurNavbar></OurNavbar>
+     <Container>
+      {JSON.stringify(problemList)}
+     </Container>
     </div>
   )
 }
